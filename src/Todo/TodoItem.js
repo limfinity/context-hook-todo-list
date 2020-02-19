fa7aa30ab2bbc00f66a27ffa09fdb83a9288b944
Peter Pult <peter.pult@netlight.com>
2020-02-19 11:37:16 +0100

    chore: make it a little prettier

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -2,17 +2,24 @@ import React from 'react';
 
 const TodoItem = ({ todo, index, toggleTodo, removeTodo }) => {
   return (
-    <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
-    >
-      {todo.text}
+    <div className="todo">
+      <span style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
+        {todo.text}
+      </span>
 
-      <div>
-        <button onClick={() => toggleTodo(index)}>
+      <div class="buttons">
+        <button
+          class="button is-rounded is-small is-primary is-outlined"
+          onClick={() => toggleTodo(index)}
+        >
           {todo.isCompleted ? 'Open' : 'Complete'}
         </button>
-        <button onClick={() => removeTodo(index)}>x</button>
+        <button
+          class="button is-rounded is-small is-danger is-outlined"
+          onClick={() => removeTodo(index)}
+        >
+          x
+        </button>
       </div>
     </div>
   );
